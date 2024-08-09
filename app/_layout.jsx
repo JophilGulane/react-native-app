c7862d9ae9cc604b5cb860aa3a9ccb2afec66136
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-08-10 02:28:18 +0800

    chore: Add Tailwind CSS configuration and plugin for styling

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Slot, Stack } from 'expo-router';
 
 const RootLayout = () => {
-    return <Slot />
+    return (
+    <Stack>
+        <Stack.Screen name="index" options={{headerShown: false}}/>
+    </Stack>)
 }
 
 export default RootLayout
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Jays!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl">Jays!</Text>
+      <StatusBar style="auto"/>
+      <Link href={"/profile"} style={{color: 'blue'}}>Go to Profile!</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
