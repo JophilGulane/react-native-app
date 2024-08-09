d0cc7097a6d5e6cc4a653e5ce23d9e6a919032bf
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-08-10 00:43:20 +0800

    asd

---
+++ app/_layout.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router';
+
+const RootLayout = () => {
+    return <Slot />
+}
+
+export default RootLayout
